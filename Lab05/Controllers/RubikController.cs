3fe642b8b26735a9cb38f2b0a85a60d6d78970a9
THietTML <93172909+THietTML@users.noreply.github.com>
2022-06-14 18:56:24 +0700

    no

---
+++ Lab05/Controllers/RubikController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lab05.Models;
+
+namespace Lab05.Controllers
+{
+    public class RubikController : Controller
+    {
+        dbRubikStoreDataContext data = new dbRubikStoreDataContext();
+        // GET: Rubik
+        public ActionResult Index()
+        {
+            var all_rubik = from s in data.Rubiks select s;
+            return View(all_rubik);
+        }
+    }
+}
